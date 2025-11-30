68abcf7644a3c0d23b4f288e9f964792c97c9382
kiri <mail@jelles.net>
2025-11-30 17:01:29 +0100

    Change todoman config

--- modules/home-manager/todoman.nix
+++ modules/home-manager/todoman.nix
@@ -1,12 +1,14 @@
-{ config, pkgs, ... }:
 {
-  home.packages = [ pkgs.todoman ];
-
-  xdg.configFile."todoman/config.py".text = ''
-    path = "${config.xdg.dataHome}/calendars/*/*"
-    date_format = "%d-%m-%Y"
-    time_format = "%H:%M"
-    default_list = "personal"
-    default_due = 48
-  '';
+  programs.todoman = {
+    enable = true;
+    glob = "*/*";
+    extraConfig = ''
+      date_format = "%Y-%m-%d"
+      time_format = "%H:%M"
+      default_list = "personal"
+      default_due = 0
+      default_command = "list --sort due"
+      humanize = True
+    '';
+  };
 }

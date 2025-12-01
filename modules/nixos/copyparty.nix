6976180fd47179550d01feb47cdd9be04fac5c3c
kiri <mail@jelles.net>
2025-12-01 01:38:40 +0100

    Update copyparty config

--- modules/nixos/copyparty.nix
+++ modules/nixos/copyparty.nix
@@ -1,3 +1,7 @@
+{ config, ... }:
+let
+  username = config.var.username;
+in
 {
   services = {
     copyparty = {
@@ -6,6 +10,39 @@
       settings = {
         e2dsa = true;
         no-cfg = true;
+      };
+
+      accounts = {
+        "${username}" = {
+          passwordFile = "/run/keys/copyparty-kiri-pass";
+        };
+      };
+
+      groups = {
+        "admin" = [ "${username}" ];
+      };
+
+      volumes = {
+        "/public" = {
+          path = "/var/lib/copyparty/public";
+
+          access = {
+            r = "*";
+            rwmd = "@admin";
+          };
+
+          flags = {
+            fk = 4;
+            scan = 60;
+          };
+        };
+
+        "/" = {
+          path = "/var/lib/copyparty/private";
+          access = {
+            rwmd = "@admin";
+          };
+        };
       };
     };
 

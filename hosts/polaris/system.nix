dca64dd6c288f888d3766278a6e906b1ec2fa8b2
kiri <mail@jelles.net>
2025-12-04 18:41:59 +0100

    Enable wake-on-lan for polaris

--- hosts/polaris/system.nix
+++ hosts/polaris/system.nix
@@ -3,9 +3,21 @@
   imports = [
     ../../modules/nixos/desktop.nix
 
+    ../../modules/nixos/ssh.nix
     ./hardware-configuration.nix
     ./variables.nix
   ];
+
+  networking = {
+    interfaces = {
+      enp5s0 = {
+        wakeOnLan.enable = true;
+      };
+    };
+    firewall = {
+      allowedUDPPorts = [ 9 ];
+    };
+  };
 
   home-manager.users."${config.var.username}" = import ./home.nix;
 

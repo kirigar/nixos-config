3d62cc10d33934eab994a8a7bb19dc678ac56ecf
kiri <mail@jelles.net>
2025-12-04 18:45:24 +0100

    Add host for work github

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -4,6 +4,15 @@
     enable = true;
     enableDefaultConfig = false;
 
+    matchBlocks = {
+      "github-work" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "/home/kiri/.ssh/github-work.pub";
+        identitiesOnly = true;
+      };
+    };
+
     includes = [
       config.sops.secrets.ssh_config_orion.path
     ];
